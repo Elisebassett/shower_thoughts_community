a158b62fa66b491b065837c60f28bfc84752f027
Elisebassett <elise@iileaders.com>
2017-11-04 18:50:24 -0600

    main pg fixes #1

--- main.rb
+++ main.rb
@@ -7,4 +7,55 @@ set :database, "sqlite3:main.sqlite3"
 get '/' do
   erb :home
 end 
-  
+
+get '/sign_in' do
+	erb :sign_in
+end
+
+get '/welcome' do
+	erb :welcome
+end
+
+get '/create_account' do 
+	erb :create_account
+end
+
+get '/profile' do
+	erb :profile
+end
+
+get '/profile_posts' do
+	erb :profile_posts
+end
+
+get '/change_details' do
+	erb :profile_posts
+end
+
+get '/post' do
+	erb :post
+end
+
+get '/find' do
+	erb :find
+end
+
+get '/found' do
+	erb :found
+end
+
+
+post '/create_account' do
+
+	erb :welcome
+end
+
+post '/sign_in' do
+	
+	erb :welcome
+end
+
+post '/find' do
+	
+	erb :found
+end
